39a48eccb99b0200479cb599f216d10f3a7c26ad
Kelsey Gilmore-Innis <kelseyq@users.noreply.github.com>
2017-12-24 14:41:46 -0800

    separate tests into challenges

--- tests/test_set1.py
+++ tests/test_set1.py
@@ -3,8 +3,8 @@ __author__ = 'Kelsey Gilmore-Innis'
 import unittest
 from set1 import challenge1, challenge2, challenge3, challenge4, challenge5, challenge6
 
-class SetOneTests(unittest.TestCase):
 
+class Challenge1(unittest.TestCase):
     def test_convert_to_base64_nonhex(self):
         self.assertRaises(ValueError, challenge1.convert_to_base64, "this is not a hex string")
 
@@ -12,6 +12,8 @@ class SetOneTests(unittest.TestCase):
         self.assertEqual(challenge1.convert_to_base64(
             "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"),
             b'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t')
+
+class Challenge2(unittest.TestCase):
 
     def test_xor_nonhex(self):
         self.assertRaises(ValueError, challenge2.fixed_xor_hex, "this is not a hex string", "this is not a hex string")
@@ -24,6 +26,9 @@ class SetOneTests(unittest.TestCase):
             "1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965"),
             bytearray.fromhex("746865206b696420646f6e277420706c6179"))
 
+
+class Challenge3(unittest.TestCase):
+
     def test_single_byte_xor(self):
         self.assertEqual(
             challenge3.single_byte_xor(bytes.fromhex("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"),
@@ -34,9 +39,14 @@ class SetOneTests(unittest.TestCase):
             challenge3.decrypt_single_byte_xor("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"),
             b"Cooking MC's like a pound of bacon")
 
+
+class Challenge4(unittest.TestCase):
+
     def test_detect_single_byte_xor(self):
         self.assertEqual(challenge4.detect_single_byte_xor(), b"Now that the party is jumping\n")
 
+
+class Challenge5(unittest.TestCase):
     def test_repeating_key_xor(self):
         import binascii
         encrypted = challenge5.encrypt_repeating_key("""Burning 'em, if you ain't quick and nimble
@@ -50,6 +60,9 @@ I go crazy when I hear a cymbal"""
         encrypted = challenge5.encrypt_repeating_key(original.encode("ascii"), "ICE")
         decrypted = challenge5.encrypt_repeating_key(encrypted, "ICE")
         self.assertEqual(original, decrypted.decode("ascii"))
+
+
+class Challenge6(unittest.TestCase):
 
     def __with_file(self, filename, function):
         import os
